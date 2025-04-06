748f741bf5096275304e636b11090a78512b50cf
Changyu Li <scycl11@nottingham.edu.cn>
2025-04-07 01:14:14 +0800

    fixed multiple errors relocated model

--- content/unreal engine tutorial/example_model/_index.md
+++ content/unreal engine tutorial/example_model/_index.md
@@ -1,9 +1,10 @@
 ---
 title: "Example Model"
-date: 2025-04-07T00:28:24+08:00
+date: 2025-04-06T23:28:24+08:00
 tags: []
 series: []
 featured: true
 ---
+Use your mouse rotate around previous scene. Use the scrolling wheel to zoom in or out.
 
-{{< threejs version="0.160.0" id="hello" src="test2.js" >}}
+{{< threejs version="0.160.0" id="hello" src="ward.js" >}}
--- content/unreal engine tutorial/example_model/ward.js
+++ content/unreal engine tutorial/example_model/ward.js
@@ -7,14 +7,20 @@ import { OrbitControls } from 'three/OrbitControls.js';
 // 初始化场景
 const scene = new THREE.Scene();
 
+const width = 1200;
+const height = 800; 
+
 // 创建相机
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 camera.position.set(-3, 2, 0);
 
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(width, height);
 document.getElementById('threejs-container-hello').appendChild(renderer.domElement);
+
 
 // 添加平行光
 const light = new THREE.DirectionalLight(0xffffff, 1);
